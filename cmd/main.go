43881cda0d1c3bd1f118eb8aa2d079100c7a233b
Alex Harvey <alex@afharvey.com>
2020-07-25 13:01:38 +0100

    test ci

--- cmd/main.go
+++ cmd/main.go
@@ -1,24 +1,9 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"net/http"
-
-	"github.com/graphql-go/graphql"
-	"github.com/graphql-go/graphql/testutil"
 )
 
 func main() {
 	fmt.Println("hello")
-}
-
-func HelloWorld(w http.ResponseWriter, r *http.Request) {
-	query := r.URL.Query().Get("query")
-	result := graphql.Do(graphql.Params{
-		Schema:        testutil.StarWarsSchema,
-		RequestString: query,
-	})
-	w.Header().Add("Content-Type", "application/json")
-	_ = json.NewEncoder(w).Encode(result)
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/afharvey/ww-sweet-shop
 
 go 1.14
 
-require github.com/graphql-go/graphql v0.7.9
+require (
+	github.com/graphql-go/graphql v0.7.9
+	github.com/mattn/goveralls v0.0.6 // indirect
+)
